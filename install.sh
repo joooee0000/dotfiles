47d2e060ac420518ab1e1e346912d7d1b08168b2
joooee0000 <joooee0000@gmail.com>
2018-09-04 21:17:02 +0900

    fix

--- install.sh
+++ install.sh
@@ -2,6 +2,5 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/indent ~/.vim/indent
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
